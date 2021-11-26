29151a9dc1cb8d11e1b8c6067399bd39aa071499
jmargutti <jacopo.margutti@gmail.com>
2021-11-26 11:45:58 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,133 @@
+import requests
+import pandas as pd
+from collections import OrderedDict
+import io
+import os
+from dotenv import load_dotenv
+# from azure.storage.blob import BlobServiceClient
+from flask import Flask, render_template, request, send_file
+app = Flask(__name__)
+load_dotenv()  # take environment variables from .env
+
+
+def process_data(df_form, bracelet_number=None):
+
+    if df_form.empty or bracelet_number is None:
+        return render_template('data.html')
+
+    df = df_form[df_form['bracelet_number'] == bracelet_number]
+
+    if df.empty:
+        return render_template('data.html',
+                               not_found=True,
+                               bracelet_number=bracelet_number)
+
+    df['date'] = pd.to_datetime(df['start']).dt.date
+
+    # generic info
+    info = {}
+    for x in ['name', 'gender', 'age']:
+        if x in df.columns:
+            info[x] = df[x].unique()[-1]
+    if 'age' in info.keys():
+        info['age'] = map_age(info['age'])
+
+    # consultations
+    consultations = []
+    for ix, row in df.iterrows():
+        consultation = {}
+        consultation['Date'] = row['date']
+        for level in ['primary', 'secondary', 'tertiary']:
+            case_key = level+'_case'
+            case_label = level.capitalize()+' Diagnosis'
+            if case_key in row.keys():
+                if not pd.isna(row[case_key]):
+                    case = row[case_key]
+                    case = case_map(case)
+                    consultation[case_label] = case
+        treatment = row['treatment']
+        if pd.isna(treatment):
+            treatment = "nothing"
+        consultation['Treatment'] = treatment
+        if row['referral'] == 'yes':
+            consultation['Referral is needed'] = row['referral_urgency']
+        consultations.append(consultation)
+
+    print(consultations)
+    return render_template('data.html',
+                           info=info,
+                           consultations=consultations)
+
+
+def get_data():
+    # get data from kobo
+    headers = {'Authorization': f'Token {os.getenv("TOKEN")}'}
+    data_request = requests.get(f'https://kobonew.ifrc.org/api/v2/assets/{os.getenv("ASSET")}/data.json', headers=headers)
+    data = data_request.json()
+    if 'results' in data.keys():
+        df_form = pd.DataFrame(data['results'])
+    else:
+        df_form = pd.DataFrame()
+    return df_form
+
+
+@app.route("/data", methods=['POST'])
+def default_page():
+    if request.form['password'] == os.getenv("PASSWORD"):
+        df_form = get_data()
+        return process_data(df_form)
+    else:
+        return render_template('home.html')
+
+
+@app.route("/dataupdate", methods=['POST'])
+def update_rescue():
+    if 'bracelet' in request.form.keys():
+        bracelet_number = request.form['bracelet']
+    else:
+        bracelet_number = None
+    df_form = get_data()
+    return process_data(df_form, bracelet_number)
+
+
+@app.route("/")
+def login_page():
+    return render_template('home.html')
+
+
+def case_map(case):
+    case_map_dict = {'male': 'Male', 'female': 'Female', 'other': 'Other', 'u1': 'Less than 1 year', '1_4': '1-4 years',
+                     '5_17': '5-17 years', '18_50': '18-50 years', '50p': '50+ years', 'scabies': 'Scabies',
+                     'sea_sickness': 'Sea sickness', 'herpes': 'Herpes lip / cold sore', 'skin': 'Other skin infection',
+                     'gastritis': 'Gastritis', 'dental': 'Dental', 'injury': 'Non-violence related injury',
+                     'violence': 'Violence related injury', 'fuel_burn': 'Fuel Burns',
+                     'exposure_skin': 'Exposure related skin disorder', 'dehydration': 'Dehydration',
+                     'hypothermia': 'Hypothermia', 'body_pain': 'Generalized body pain / headache', 'awd': 'Acute watery diarrhoea',
+                     'sawd': 'Severe acute diarrhoea', 'abd': 'Acute bloody diarrhoea',
+                     'fever': 'Fever without identified cause', 'urti': 'Acute upper respiratory tract infection',
+                     'lrti': 'Acute lower respiratory tract infection', 'tb': 'Tuberculosis (suspected)',
+                     'meningitis': 'Meningitis (suspected)', 'std': 'Sexually transmitted infection (suspected)',
+                     'uti': 'Urinary tract infection', 'eye': 'Eye infection', 'gyno': 'Gynaecological disease',
+                     'anaemia': 'Anaemia', 'malnutrition': 'Severe acute malnutrition', 'chronic': 'Chronic disease',
+                     'cpd': 'Mental health presentation', 'spd': 'Severe psychiatric disorder',
+                     'covid': 'Confirmed COVID-19', 'sv': 'SV/SGBV', 'pregnancy': 'Pregnancy related (ANC & PNC)',
+                     'baby': 'Baby consultation', 'yes': 'yes', 'no': 'no'}
+    if case in case_map_dict.keys():
+        return case_map_dict[case]
+    else:
+        return case
+
+
+def map_age(age):
+    age_dict = {'u1': 'Less than 1 year',
+                '1_4': '1-4 years',
+                '5_12': '5-12 years',
+                '13_17': '13-17 years',
+                '18_50': '18-50 years',
+                '50p': '50+ years'}
+    if age in age_dict.keys():
+        return age_dict[age]
+    else:
+        return age
+
+

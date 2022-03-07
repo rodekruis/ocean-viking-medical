b5a8f8083795bd3499063306f6a4faa2f649ef8d
jmargutti <jacopo.margutti@gmail.com>
2022-03-07 12:58:33 +0100

    update requirements, minor bug fixes

--- app.py
+++ app.py
@@ -48,17 +48,13 @@ def process_data(df_form, bracelet_number=None, first=False):
                                not_found=True,
                                bracelet_number=bracelet_number)
 
-    # print(df.head(10))
     df['start'] = pd.to_datetime(df['start'])
-    # for ix, row in df.iterrows():
-    #     print(row['start'])
-    #     pd.to_datetime(row['start']).dt.date
 
     # patient info
     info = {}
     for x in ['name', 'gender', 'age']:
         if x in df.columns:
-            info[x] = df[x].unique()[-1]
+            info[x] = df[x].dropna().unique()[-1]
     if 'age' in info.keys():
         info['age'] = map_age(info['age'])
 
@@ -68,14 +64,20 @@ def process_data(df_form, bracelet_number=None, first=False):
     for ix, row in df.iterrows():
         consultation = {}
         consultation['Date'] = row['start'].date()
+        consultation['Diagnosis'] = ""
         for level in ['primary', 'secondary', 'tertiary']:
             case_key = level+'_case'
-            case_label = level.capitalize()+' Diagnosis'
+            # case_label = level.capitalize()+' Diagnosis'
             if case_key in row.keys():
                 if not pd.isna(row[case_key]):
                     case = row[case_key]
                     case = case_map(case)
-                    consultation[case_label] = case
+                    if case == "Other":
+                        case = row[level + '_case_other']
+                    if consultation['Diagnosis'] == "":
+                        consultation['Diagnosis'] = case
+                    else:
+                        consultation['Diagnosis'] = consultation['Diagnosis'] + ", " + case
         treatment = row['treatment']
         if pd.isna(treatment):
             treatment = "nothing"
@@ -86,7 +88,7 @@ def process_data(df_form, bracelet_number=None, first=False):
                 consultation['Other information'] = info_text
         if ix == len(df)-1:
             if row['referral'] == 'yes':
-                referral['Referral is needed:'] = row['referral_urgency']
+                referral['Referral is needed:'] = row['referral_urgency'].replace('_', ' ')
             else:
                 referral['Referral is not needed'] = ''
         consultations.append(consultation)
@@ -106,31 +108,33 @@ def get_data():
     headers = {'Authorization': f'Token {os.getenv("TOKEN")}'}
     data_request = requests.get(f'https://kobonew.ifrc.org/api/v2/assets/{os.getenv("ASSET")}/data.json', headers=headers)
     data = data_request.json()
+
+    # get rotation info
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+    SAMPLE_SPREADSHEET_ID = '1L-d0lT2s7QjxlXbvYkcBWdSPFkKsYEtD52J_H4VK8dA'
+    SAMPLE_RANGE_NAME = 'Rotations!A:C'
+    sa_file = 'google-service-account-hspatsea-ocean-viking.json'
+    creds = service_account.Credentials.from_service_account_file(sa_file, scopes=SCOPES)
+    service = build('sheets', 'v4', credentials=creds)
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=SAMPLE_RANGE_NAME).execute()
+    values = result.get('values', [])
+    df = pd.DataFrame.from_records(values[1:], columns=values[0])
+    df['Start date'] = pd.to_datetime(df['Start date'], dayfirst=True)
+    df['End date'] = pd.to_datetime(df['End date'], dayfirst=True)
+    df['Rotation No'] = df['Rotation No'].astype(int)
+    rotation_no = max(df['Rotation No'])
+    start_date_ = date.today()
+    end_date_ = date.today()
+
     if 'results' in data.keys():
         df_form = pd.DataFrame(data['results'])
         if df_form.empty:
-            return df_form
+            return df_form, rotation_no
         df_form['_submission_time'] = pd.to_datetime(df_form['_submission_time'])
 
-        # filter by rotation number
-        SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-        SAMPLE_SPREADSHEET_ID = '1L-d0lT2s7QjxlXbvYkcBWdSPFkKsYEtD52J_H4VK8dA'
-        SAMPLE_RANGE_NAME = 'Rotations!A:C'
-        sa_file = 'google-service-account-hspatsea-ocean-viking.json'
-        creds = service_account.Credentials.from_service_account_file(sa_file, scopes=SCOPES)
-        service = build('sheets', 'v4', credentials=creds)
-        # Call the Sheets API
-        sheet = service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                    range=SAMPLE_RANGE_NAME).execute()
-        values = result.get('values', [])
-        df = pd.DataFrame.from_records(values[1:], columns=values[0])
-        df['Start date'] = pd.to_datetime(df['Start date'], dayfirst=True)
-        df['End date'] = pd.to_datetime(df['End date'], dayfirst=True)
-        df['Rotation No'] = df['Rotation No'].astype(int)
-        rotation_no = max(df['Rotation No'])+1
-        start_date_ = date.today()
-        end_date_ = date.today()
         for ix, row in df.iterrows():
             if row['Start date'] <= pd.to_datetime(date.today()) <= row['End date']:
                 rotation_no = row['Rotation No']
@@ -141,10 +145,10 @@ def get_data():
         if not df_form.empty:
             df_form['rotation_no'] = rotation_no
         else:
-            df_form = df_form.append(pd.Series({'rotation_no': rotation_no}), ignore_index=True)
+            df_form = pd.DataFrame()
     else:
         df_form = pd.DataFrame()
-    return df_form
+    return df_form, rotation_no
 
 
 def process_summary(df_form):
@@ -152,15 +156,68 @@ def process_summary(df_form):
     process data to show summary of morbidities
     """
     morbidities_all, referral_all = {}, {}
-    patients = df_form['bracelet_number'].nunique()
-    labels = ['male', 'female', 'u5', 'total']
+    consultations = len(df_form)
+    if 'bracelet_number' not in df_form.columns:
+        consultations = 0
+        patients = 0
+        morbidities_all_ordered = {}
+        referral_all = {}
+    else:
+        patients = df_form['bracelet_number'].nunique()
+        labels = ['male', 'female', 'u5', 'total']
 
-    for bracelet in df_form['bracelet_number'].unique():
-        df = df_form[df_form['bracelet_number'] == bracelet]
+        for bracelet in df_form['bracelet_number'].unique():
+            df = df_form[df_form['bracelet_number'] == bracelet]
 
-        morbidities = {}
-        referrals = {}
-        for ix, row in df.iterrows():
+            morbidities = {}
+            referrals = {}
+            for ix, row in df.iterrows():
+                for level in ['primary', 'secondary', 'tertiary']:
+                    case_key = level + '_case'
+                    if case_key in row.keys():
+                        if not pd.isna(row[case_key]):
+                            case = row[case_key]
+                            case = case_map(case)
+                            if case == "Other":
+                                case = row[level+'_case_other']
+                            if row['age'] == 'u1' or row['age'] == '1_4':
+                                label = 'u5'
+                            else:
+                                if row['gender'] == 'male':
+                                    label = 'male'
+                                elif row['gender'] == 'female':
+                                    label = 'female'
+                            morbidities[case] = {label: 1, 'total': 1}
+                if row['referral'] == 'yes':
+                    referrals['Referrals needed'] = 1
+                    urgency = row['referral_urgency'].replace('_', ' ').capitalize()
+                    referrals[urgency] = 1
+
+            for key, morb_dict in morbidities.items():
+                if key in morbidities_all.keys():
+                    morb_dict_all = morbidities_all[key]
+                    for label in morb_dict.keys():
+                        morb_dict_all[label] = morb_dict_all[label] + 1
+                    morbidities_all[key] = morb_dict_all
+                else:
+                    for label in labels:
+                        if label not in morb_dict.keys():
+                            morb_dict[label] = 0
+                    morbidities_all[key] = morb_dict
+
+            for key in referrals.keys():
+                if key in referral_all.keys():
+                    referral_all[key] = referral_all[key] + 1
+                else:
+                    referral_all[key] = 1
+
+        # add data of patients without bracelet number
+        df_no_bracelet = df_form[pd.isna(df_form['bracelet_number'])].groupby(['age', 'gender']).last().reset_index()
+        patients = patients + len(df_no_bracelet)
+
+        for ix, row in df_no_bracelet.iterrows():
+            morbidities = {}
+            referrals = {}
             for level in ['primary', 'secondary', 'tertiary']:
                 case_key = level + '_case'
                 if case_key in row.keys():
@@ -181,79 +238,35 @@ def process_summary(df_form):
                 urgency = row['referral_urgency'].replace('_', ' ').capitalize()
                 referrals[urgency] = 1
 
-        for key, morb_dict in morbidities.items():
-            if key in morbidities_all.keys():
-                morb_dict_all = morbidities_all[key]
-                for label in morb_dict.keys():
-                    morb_dict_all[label] = morb_dict_all[label] + 1
-                morbidities_all[key] = morb_dict_all
-            else:
-                for label in labels:
-                    if label not in morb_dict.keys():
-                        morb_dict[label] = 0
-                morbidities_all[key] = morb_dict
+            for key, morb_dict in morbidities.items():
+                if key in morbidities_all.keys():
+                    morb_dict_all = morbidities_all[key]
+                    for label in morb_dict.keys():
+                        morb_dict_all[label] = morb_dict_all[label] + 1
+                    morbidities_all[key] = morb_dict_all
+                else:
+                    for label in labels:
+                        if label not in morb_dict.keys():
+                            morb_dict[label] = 0
+                    morbidities_all[key] = morb_dict
 
-        for key in referrals.keys():
-            if key in referral_all.keys():
-                referral_all[key] = referral_all[key] + 1
-            else:
-                referral_all[key] = 1
+            for key in referrals.keys():
+                if key in referral_all.keys():
+                    referral_all[key] = referral_all[key] + 1
+                else:
+                    referral_all[key] = 1
 
-    # add data of patients without bracelet number
-    df_no_bracelet = df_form[pd.isna(df_form['bracelet_number'])].groupby(['age', 'gender']).last().reset_index()
-    patients = patients + len(df_no_bracelet)
-
-    for ix, row in df_no_bracelet.iterrows():
-        morbidities = {}
-        referrals = {}
-        for level in ['primary', 'secondary', 'tertiary']:
-            case_key = level + '_case'
-            if case_key in row.keys():
-                if not pd.isna(row[case_key]):
-                    case = row[case_key]
-                    case = case_map(case)
-                    label = "other"
-                    if row['age'] == 'u1' or row['age'] == '1_4':
-                        label = 'u5'
-                    else:
-                        if row['gender'] == 'male':
-                            label = 'male'
-                        elif row['gender'] == 'female':
-                            label = 'female'
-                    morbidities[case] = {label: 1, 'total': 1}
-        if row['referral'] == 'yes':
-            referrals['Referrals needed'] = 1
-            urgency = row['referral_urgency'].replace('_', ' ').capitalize()
-            referrals[urgency] = 1
-
-        for key, morb_dict in morbidities.items():
-            if key in morbidities_all.keys():
-                morb_dict_all = morbidities_all[key]
-                for label in morb_dict.keys():
-                    morb_dict_all[label] = morb_dict_all[label] + 1
-                morbidities_all[key] = morb_dict_all
-            else:
-                for label in labels:
-                    if label not in morb_dict.keys():
-                        morb_dict[label] = 0
-                morbidities_all[key] = morb_dict
-
-        for key in referrals.keys():
-            if key in referral_all.keys():
-                referral_all[key] = referral_all[key] + 1
-            else:
-                referral_all[key] = 1
-
-    # sort morbidities by total
-    morbidities_all_ordered = OrderedDict()
-    totals = {}
-    for key, value in morbidities_all.items():
-        totals[key] = value['total']
-    sorted_totals = dict(sorted(totals.items(), key=operator.itemgetter(1), reverse=True))
-    for key in sorted_totals.keys():
-        morbidities_all_ordered[key] = morbidities_all[key]
+        # sort morbidities by total
+        morbidities_all_ordered = OrderedDict()
+        totals = {}
+        for key, value in morbidities_all.items():
+            totals[key] = value['total']
+        sorted_totals = dict(sorted(totals.items(), key=operator.itemgetter(1), reverse=True))
+        for key in sorted_totals.keys():
+            morbidities_all_ordered[key] = morbidities_all[key]
 
     return render_template('summary.html',
+                           consultations=consultations,
                            patients=patients,
                            morbidities=morbidities_all_ordered,
                            referrals=referral_all)
@@ -265,7 +278,7 @@ def update_submission():
     update referral status
     """
     bracelet_number = request.form['bracelet']
-    df_form = get_data()
+    df_form, rotation_no = get_data()
     df = df_form[df_form['bracelet_number'] == bracelet_number].reset_index(drop=True)
 
     if df.empty:
@@ -318,8 +331,65 @@ def update_submission():
             headers=headers
         )
 
-    df_form = get_data()
+    df_form, rotation_no = get_data()
     return process_data(df_form, bracelet_number)
+
+
+@app.route("/downloaddata", methods=['POST'])
+def download_data():
+    df_form, rotation_no = get_data()
+
+    # keep only referrals
+    df_form = df_form[df_form['referral'] == 'yes']
+
+    # change age
+    df_form['age'] = df_form['age'].apply(map_age)
+
+    # merge diagnoses
+    df_form['diagnosis'] = df_form['primary_case']
+    for ix, row in df_form.iterrows():
+        diagnosis = case_map(row['diagnosis'])
+        if 'secondary_case' in df_form.columns:
+            if not pd.isna(row['secondary_case']):
+                diagnosis = diagnosis + ", " + case_map(row['secondary_case'])
+        if 'tertiary_case' in df_form.columns:
+            if not pd.isna(row['tertiary_case']):
+                diagnosis = diagnosis + ", " + case_map(row['tertiary_case'])
+        df_form.at[ix, 'diagnosis'] = diagnosis
+    df_form = df_form.drop(columns=['primary_case', 'secondary_case', 'tertiary_case'])
+
+    # keep only meaningful columns
+    columns_to_keep = ['_submission_time',
+                       'bracelet_number',
+                       'name',
+                       'gender',
+                       'age',
+                       'diagnosis',
+                       'treatment',
+                       'info',
+                       'referral_urgency']
+    columns_to_keep = [x for x in columns_to_keep if x in df_form.columns]
+    df_form = df_form[columns_to_keep]
+
+    # order by bracelet number
+    df_form['bracelet_number'] = df_form['bracelet_number'].astype(float, errors='ignore')
+    df_form = df_form.sort_values(by='bracelet_number')
+
+    data_path = 'referral-data.xlsx'
+    if os.path.exists(data_path):
+        os.remove(data_path)
+    writer = pd.ExcelWriter(data_path, engine='xlsxwriter')
+    df_form.to_excel(writer, sheet_name='DATA', index=False)  # send df to writer
+    worksheet = writer.sheets['DATA']  # pull worksheet object
+    for idx, col in enumerate(df_form.columns):  # loop through all columns
+        series = df_form[col]
+        max_len = max((
+            series.astype(str).map(len).max(),  # len of largest item
+            len(str(series.name))  # len of column name/header
+        )) + 1
+        worksheet.set_column(idx, idx, max_len)  # set column width
+    writer.save()
+    return send_file(data_path, as_attachment=True, attachment_filename=data_path)
 
 
 @app.route("/data", methods=['POST'])
@@ -328,7 +398,7 @@ def default_page():
     show patient information
     """
     if request.form['password'] == os.getenv("PASSWORD"):
-        df_form = get_data()
+        df_form, rotation_no = get_data()
         return process_data(df_form, first=True)
     else:
         return render_template('home.html')
@@ -343,7 +413,7 @@ def update_bracelet():
         bracelet_number = request.form['bracelet']
     else:
         bracelet_number = None
-    df_form = get_data()
+    df_form, rotation_no = get_data()
     return process_data(df_form, bracelet_number)
 
 
@@ -352,7 +422,7 @@ def summary():
     """
     show summary of morbidities
     """
-    df_form = get_data()
+    df_form, rotation_no = get_data()
     return process_summary(df_form)
 
 
